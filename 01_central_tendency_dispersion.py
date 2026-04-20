730dab8d1d9cfea2304a39fac28926af51091c06
Snikitha Grandhe <153828491+SnikithaD2@users.noreply.github.com>
2026-04-20 21:53:05 +0530

    Update 01_central_tendency_dispersion.py

--- 01_central_tendency_dispersion.py
+++ 01_central_tendency_dispersion.py
@@ -8,24 +8,17 @@
 # consistency. They are widely used in data analysis, machine learning, and decision-making
 # to interpret numerical datasets effectively and draw meaningful conclusions from data.
 
-import pandas as pd
-from sklearn.datasets import load_iris
+import statistics as stats
 
-dataset = load_iris()
-df = pd.DataFrame(data=dataset.data, columns=dataset.feature_names)
-
-mean_val = df.mean()
-median_val = df.median()
-mode_val = df.mode().iloc[0]
-std_dev = df.std()
-variance = df.var()
-
-stats_df = pd.DataFrame({
-    'Mean': mean_val,
-    'Median': median_val,
-    'Mode': mode_val,
-    'Std Dev': std_dev,
-    'Variance': variance
-})
-
-print(stats_df)
+arr= list(map(int,input("Enter the numbers:").split()))
+n = len(arr)
+mean = stats.mean(arr)
+median = stats.median(arr)
+mode = stats.multimode(arr)
+variance = stats.variance(arr)
+standard_deviation = stats.stdev(arr)
+print("Mean =", mean)
+print("Median =", median)
+print("Mode =", mode)
+print("Variance =", variance) 
+print("Standard Deviation =", standard_deviation)
